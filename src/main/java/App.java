a35b8d12f1ffc2247eed5039b729adbb0738c26c
Pavel Glavatskikh <byba1100@mail.ru>
2023-09-11 16:06:47 +0400

    bean prototype

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+
+        System.out.println(cat.getName());
+        System.out.println(cat1.getName());
+
+        System.out.println(cat==cat1);
+
+        System.out.println(bean==bean2);
+
+
+
+
     }
 }
